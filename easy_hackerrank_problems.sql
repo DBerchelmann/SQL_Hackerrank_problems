cc4c97189abe6ac70d765cf66dfecebe3d2719e0
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-22 10:57:43 -0600

    added addiitonal problems/solutions

--- easy_hackerrank_problems.sql
+++ easy_hackerrank_problems.sql
@@ -4,3 +4,21 @@
 SELECT DISTINCT city
 FROM station
 WHERE mod(id,2)=0;
+
+# Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+
+SELECT (COUNT(city) - (COUNT(DISTINCT(city))))
+FROM station;
+
+# Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+# If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
+SELECT DISTINCT city, LENGTH(city)
+FROM station
+ORDER BY LENGTH(city) ASC
+LIMIT 1;
+
+SELECT DISTINCT city, LENGTH(city)
+FROM station
+ORDER BY LENGTH(city) DESC
+LIMIT 1;
