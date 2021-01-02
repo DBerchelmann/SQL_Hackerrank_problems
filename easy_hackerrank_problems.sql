b4ae9334a5b9fa9ec08f95f35699a91b30287292
David Berchelmann <david.a.berchelmann@gmail.com>
2021-01-02 17:48:43 -0600

    added additional JOIN problems

--- easy_hackerrank_problems.sql
+++ easy_hackerrank_problems.sql
@@ -144,3 +144,16 @@ FROM CITY;
 SELECT SUM(city.population)
 FROM city JOIN country ON (city.countrycode=country.code)
 WHERE continent = 'Asia';
+
+# Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+
+SELECT city.name
+FROM city JOIN country ON city.countrycode=country.code
+WHERE continent = 'Africa';
+
+# Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and 
+# their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+SELECT country.continent, FLOOR(AVG(city.population))
+FROM city JOIN country ON city.countrycode=country.code
+GROUP BY country.continent;
