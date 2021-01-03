e00249873d241cd96fafd3afd8ede7d5a1807909
David Berchelmann <david.a.berchelmann@gmail.com>
2021-01-02 23:23:17 -0600

    added more SQL problems, including a medium level

--- easy_hackerrank_problems.sql
+++ easy_hackerrank_problems.sql
@@ -157,3 +157,40 @@ WHERE continent = 'Africa';
 SELECT country.continent, FLOOR(AVG(city.population))
 FROM city JOIN country ON city.countrycode=country.code
 GROUP BY country.continent;
+
+# Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 37.7880  and less than 137.2345 . Truncate your answer to  decimal places.
+
+SELECT ROUND(sum(LAT_N),4)
+FROM STATION 
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+
+# Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345 . Round your answer to 4 decimal places.
+
+SELECT ROUND(LONG_W,4)
+FROM STATION 
+WHERE LAT_N = (SELECT max(LAT_N) 
+               FROM station 
+               WHERE LAT_N < 137.2345);
+
+# Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than  38.7780. Round your answer to 4 decimal places.
+
+SELECT ROUND((LONG_W),4)
+FROM station 
+WHERE LAT_N = (SELECT MIN(LAT_N) 
+               FROM station 
+               WHERE LAT_N > 38.7780);
+
+# Manhattan Distance Quesiton
+
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+
+ # a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ # b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ # c  happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ # d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+# Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+
+# Manhattan formula is (a-c) m+ (b-d)
+
+SELECT ABS(ROUND((MIN(LAT_N) - MAX(LAT_N)),4) + ROUND((MIN(LONG_W) - MAX(LONG_W)),4)) 
+FROM station;
